9a03c24ebee5fb2cbf470bd79151967c980f731d
Unchaptered <workstation19961002@gmail.com>
2022-01-24 08:00:22 +0900

    ADD views components

--- src/routers/homeRouter.js
+++ src/routers/homeRouter.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { getHome } from "../controllers/homeControllers.js";
 
 const HOME_ROUTER=express.Router();
+
+HOME_ROUTER.route("/").get(getHome);
 
 export default HOME_ROUTER;
