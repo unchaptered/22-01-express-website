3cf7f8e060ec0ca4880fac353580a80f9165c3ee
Unchaptered <workstation19961002@gmail.com>
2022-01-28 18:09:55 +0900

    FIX created date valeu in uesrModel.js

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -5,8 +5,8 @@ const userSchema=new mongoose.Schema({
     username: { type:String, required:true },
     userid: { type:String, required:true, unique:true },
     userpw: { type:String, required:true },
-    usercreated:  { type:Object, required:true },
-})
+    createdDate:  { type:Object, required:true }
+});
 
 userSchema.pre("save", async function() {
     if(this.isModified("userpw")) {
