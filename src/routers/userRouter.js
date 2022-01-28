26297f300c88ef578f41e109be47993525c4defd
Unchaptered <workstation19961002@gmail.com>
2022-01-28 18:06:49 +0900

    ADD connection to getUserList, getUserRemove

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { getJoin, getLogin, postJoin, postLogin, getLogout, getProfile, postProfile } from "../controllers/userControllers.js";
-import { preventLoginUser, preventLogoutUser } from "../middleware.js";
+import { getUserLists, getJoin, getLogin, postJoin, postLogin, getLogout, getProfile, postProfile, getUserRemove } from "../controllers/userControllers.js";
+import { preventLoginUser, preventLogoutUser, preventNonOwner } from "../middleware.js";
 
 const USERS_ROUTER=express.Router();
+USERS_ROUTER.route("/").get(getUserLists);
 USERS_ROUTER.route("/join").all(preventLoginUser).get(getJoin).post(postJoin);
 USERS_ROUTER.route("/login").all(preventLoginUser).get(getLogin).post(postLogin);
 USERS_ROUTER.route("/logout").all(preventLogoutUser).get(getLogout);
-USERS_ROUTER.route("/:id").get(getProfile).post(postProfile);
+
+USERS_ROUTER.route("/:id").get(getProfile).all(preventNonOwner).post(postProfile);
+USERS_ROUTER.route("/:id/remove").all(preventNonOwner).get(getUserRemove);
 
 export default USERS_ROUTER;
